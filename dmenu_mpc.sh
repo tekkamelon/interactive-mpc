782056ef52ec0d1433490ac09d22984a54d790c2
tekkamelon <type2guntank@gmail.com>
2023-04-12 01:03:31 +0900

    デフォルトの設定の追加など
    dmenuのデフォルトのフォント,プロンプト,行数の設定
    一部のコマンドにプロンプトを設定
    コメントの修正

--- dmenu_mpc.sh
+++ dmenu_mpc.sh
@@ -48,8 +48,9 @@ EOS
 )
 
 # スクリプト本体の起動時の引数をdmenuに渡す
-dmenu_custom="dmenu $@"
-# dmenu_custom="rofi -dmenu"
+# デフォルトはフォントを"monospace"プロンプトに"dmenu_mpc",13行で表示
+# dmenu_custom="dmenu -fn "monospace" -p "dmenu_mpc" -l 13 ${@}"
+dmenu_custom="rofi -dmenu"
 
 # コマンド一覧を${dmenu_custom}で表示,選択されたコマンドを代入
 selected_command=$(echo "${command}" | ${dmenu_custom})
@@ -60,7 +61,7 @@ case "${selected_command}" in
 	# ステータスの表示
 	"status" ) mpc status ;;
 
-	# toggle,next,prev,random,repeat,stop,clearの場合,選択結果をmpcに渡し出力を抑制
+	# toggle,next,prev,random,repeat,stop,clearの場合,選択結果をmpcに渡す
 	"toggle" | "next" | "prev" | "random" | "repeat" | "stop" | "clear" ) mpc "${selected_command}" ;;
 
 	# playlistの場合,キュー内の曲に行番号を付与して出力,dmenuへ渡し,選択結果の行番号のみmpcに渡す
@@ -73,7 +74,7 @@ case "${selected_command}" in
 	"listplaylist" ) mpc lsplaylist | ${dmenu_custom} | xargs mpc load ;;
 
 	# change_HOSTの場合,dmneuから受け取った入力を変数に代入
-	"change_HOST" ) input_host=$(echo "please Enter hostname or IP adress"  | ${dmenu_custom})
+	"change_HOST" ) input_host=$(cat /tmp/hostname | ${dmenu_custom} -p "please Enter hostname or IP adress")
 		
 		# "input_host"で疎通確認,成功で真,失敗で偽
 		if mpc --host="${input_host}" ; then 
@@ -87,12 +88,12 @@ case "${selected_command}" in
 		else
 
 			# 偽の場合はメッセージを表示,dmenuに渡す
-			echo "failed to resolve hostname!" | ${dmenu_custom}
+			echo "failed to resolve hostname!" | ${dmenu_custom} > /dev/null
 
 		fi ;;
 
 	# change_PORTの場合,dmneuから受け取った入力を変数に代入
-	"change_PORT" ) input_port=$(echo "please Enter number of port"  | ${dmenu_custom})
+	"change_PORT" ) input_port=$(cat /tmp/port  | ${dmenu_custom} -p "please Enter number of port")
 		
 		# "input_host"で疎通確認,成功で真,失敗で偽
 		if mpc --port="${input_port}" ; then 
@@ -106,14 +107,14 @@ case "${selected_command}" in
 		else
 
 			# 偽の場合はメッセージを表示,dmenuに渡す
-			echo "failed to resolve number of port!" | ${dmenu_custom}
+			echo "failed to resolve number of port!" | ${dmenu_custom} > /dev/null
 
 		fi ;;
 
-	# 上記のいずれでもない場合
+	# 上記のいずれでもない場合はメッセージを表示
 	* ) echo "command not found" | ${dmenu_custom} ;;
 
-esac | 
+esac |
 
 # 各コマンドの結果をdmenuに渡し,選択結果を捨てる
 ${dmenu_custom} > /dev/null
