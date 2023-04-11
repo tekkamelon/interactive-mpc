0490fb113ccab95403d77d86fb9903c07ce97199
tekkamelon <type2guntank@gmail.com>
2023-04-11 21:32:17 +0900

    軽微な修正
    コメントの修正
    実行結果をdmenuに渡す

--- dmenu_mpc.sh
+++ dmenu_mpc.sh
@@ -16,7 +16,7 @@ listall
 EOS
 )
 
-# dmenuの設定
+# スクリプト本体の起動時の引数をdmenuに渡す
 dmenu_custom="dmenu $@"
 
 # コマンド一覧を${dmenu_custom}で表示,選択されたコマンドを代入
@@ -26,19 +26,25 @@ selected_command=$(echo "${command}" | ${dmenu_custom})
 case "${selected_command}" in
 
 	# ステータスの表示
-	"status" ) mpc status | ${dmenu_custom} ;;
+	"status" ) mpc status ;;
 
-	# toggle,next,prev,random,repeat,stop,clearの処理
-	"toggle" | "next" | "prev" | "random" | "repeat" | "stop" | "clear" ) echo "${selected_command}" | xargs mpc ;;
+	# toggle,next,prev,random,repeat,stop,clearの場合,選択結果をmpcに渡し出力を抑制
+	"toggle" | "next" | "prev" | "random" | "repeat" | "stop" | "clear" ) mpc "${selected_command}" ;;
 
-	# playlistの処理
-	"playlist" ) mpc playlist | nl -s":" | ${dmenu_custom} | cut -d":" -f1 | xargs mpc play ;;
+	# playlistの場合,キュー内の曲に行番号を付与して出力,dmenuへ渡し,選択結果の行番号のみmpcに渡す
+	"playlist" ) mpc playlist | grep -n . | ${dmenu_custom} | cut -d":" -f1 | xargs mpc play ;;
 
-	# listallの処理
+	# listallの場合,曲一覧をdmenuに渡し,選択結果をmpcに渡す
 	"listall" ) mpc listall | ${dmenu_custom} | mpc insert && mpc next ;;
 
-	# lsplaylistの処理
+	# lsplaylistの場合,プレイリスト一覧をdmenuに渡し,選択結果をmpcに渡す
 	"listplaylist" ) mpc lsplaylist | ${dmenu_custom} | xargs mpc load ;;
 
-esac
+	# 上記のいずれでもない場合
+	* ) echo "command not found" | ${dmenu_custom} ;;
+
+esac | 
+
+# 各コマンドの結果をdmenuに渡し,選択結果を捨てる
+${dmenu_custom} > /dev/null
 
